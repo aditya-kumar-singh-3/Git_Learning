f22c369c82d246843e8820d64d0a9fdfe190b727
Aditya kumar singh <kumaraditya30102003@gmail.com>
2024-09-12 18:41:32 +0530

    Changes

---
+++ aditya.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aditya = () => {
+  return (
+    <div>
+      <h1>hfjfndof</h1>
+    </div>
+  );
+}
+
+export default aditya;
--- init.js
+++ init.js
@@ -39,3 +39,5 @@ let allChats =  [{
 ];
 
 Chat.insertMany(allChats);
+
+
